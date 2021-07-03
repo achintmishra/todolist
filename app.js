36347653963731a3276e269c556d1b8da91ab0a6
achintmishra <achintaryamishra@gmail.com>
2021-07-04 02:37:56 +0530

    heroku ports

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -133,6 +133,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+app.listen(port,()=>{
+  console.log("your server is up on port"+port)
+})
